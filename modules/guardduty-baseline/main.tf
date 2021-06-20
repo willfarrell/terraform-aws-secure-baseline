3026cbfc431558697a317fdc370a8d6897f4db02
nozaq <code@nozaq.com>
2021-06-20 19:52:20 +0900

    refactor: use `count` to toggle guardduty-baseline

--- modules/guardduty-baseline/main.tf
+++ modules/guardduty-baseline/main.tf
@@ -3,8 +3,6 @@
 # --------------------------------------------------------------------------------------------------
 
 resource "aws_guardduty_detector" "default" {
-  count = var.enabled ? 1 : 0
-
   enable                       = true
   finding_publishing_frequency = var.finding_publishing_frequency
 
@@ -12,9 +10,9 @@ resource "aws_guardduty_detector" "default" {
 }
 
 resource "aws_guardduty_member" "members" {
-  count = var.enabled ? length(var.member_accounts) : 0
+  count = length(var.member_accounts)
 
-  detector_id = aws_guardduty_detector.default[0].id
+  detector_id = aws_guardduty_detector.default.id
   invite      = true
 
   account_id                 = var.member_accounts[count.index].account_id
@@ -24,8 +22,8 @@ resource "aws_guardduty_member" "members" {
 }
 
 resource "aws_guardduty_invite_accepter" "master" {
-  count = var.enabled && var.master_account_id != "" ? 1 : 0
+  count = var.master_account_id != "" ? 1 : 0
 
-  detector_id       = aws_guardduty_detector.default[0].id
+  detector_id       = aws_guardduty_detector.default.id
   master_account_id = var.master_account_id
 }
